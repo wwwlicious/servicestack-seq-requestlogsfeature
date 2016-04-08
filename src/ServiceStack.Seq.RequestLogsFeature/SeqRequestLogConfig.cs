1fd6d7b7feb8aa321e8845a1c400f908c328800d
Scott Mackay <wwwlicious@users.noreply.github.com>
2016-04-08 11:35:37 +0100

    Merging changes to surface config settings for logging options (#5)
    * Allow toggling of settings at run-time. Add raw event Action to piggyback on datastream if populated (always executes even if !Enabled)
    
    * IsRequestLog always pushed to Seq

---
+++ src/ServiceStack.Seq.RequestLogsFeature/SeqRequestLogConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace ServiceStack.Seq.RequestLogsFeature
+{
+    [Route("/SeqRequestLogConfig")]
+    public class SeqRequestLogConfig : IReturn<SeqRequestLogConfig>
+    {
+        public bool? Enabled { get; set; }
+        public bool? EnableSessionTracking { get; set; }
+        public bool? EnableRequestBodyTracking { get; set; }
+        public bool? EnableResponseTracking { get; set; }
+        public bool? EnableErrorTracking { get; set; }
+    }
+}
